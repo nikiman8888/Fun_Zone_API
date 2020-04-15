f3d34be46ad209e982901eb7c0724dbc745962d0
Nikolay <nikiman88@yahoo.com>
2020-04-15 14:00:05 +0300

    First commit

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 9999,
-        dbURL: 'mongodb://localhost:27017/Soccer-Prediction',
+        dbURL: 'mongodb://localhost:27017/Game_App',
         authCookieName: 'x-auth-token'
     },
     production: {}
